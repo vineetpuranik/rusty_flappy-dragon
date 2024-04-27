e4a3489c7acc4bac4a3519f52613104a00bf6730
Vineet Puranik <vnt.manu@gmail.com>
2024-04-27 12:03:15 -0700

    Adding Main Menu, Playing and and End modes

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -13,18 +13,68 @@ struct State {
 }
 
 impl State {
+    // constructor for our struct
     fn new() -> Self {
         State {
             mode: GameMode::Menu,
         }
     }
+
+    // restart the game
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+    // play function
+    fn play(&mut self, ctx: &mut BTerm) {
+        // TODO: Fill in this stub later
+        self.mode = GameMode::End;
+    }
+
+    // main_menu function
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to flappy dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {},
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.mode = GameMode::Menu,
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {},
+            }
+        }
+
+    }
+
+
 }
 
 //implement the GameState trait
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello Bracket Terminal");
+        // match the current mode and call appropriate functions.
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
+        }
     }
 }
 
